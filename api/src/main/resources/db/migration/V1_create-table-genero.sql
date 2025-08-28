ae0b478c75bca65dda39b9edbd9834ff2885dc93
Lucast34 <lucast34t@gmail.com>
2025-08-28 11:51:25 -0300

    Create genero and Lead

---
+++ api/src/main/resources/db/migration/V1_create-table-genero.sql
@@ -0,0 +1,6 @@
+create table if not exists tb_genero(
+    id long auto_increment,
+    nome varchar(50)
+
+    primary key(id)
+);
---
+++ api/src/main/resources/db/migration/V1_create-table-lead.sql
@@ -0,0 +1,7 @@
+create table tb_lead(
+    id bigint not null auto_increment,
+    nome varchar(100) not null,
+    email varchar(100) not null,
+    telefone varchar(100) not null,
+    primary key(id)
+)
